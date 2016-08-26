5c998169bbed3590b8992260815cee889fb8f3ae
John Kelley <scouting4jesus@gmail.com>
2016-08-26 13:14:01 -0700

    FizzBuzz printing added
    Basic FizzBuzz program up and running. Added the basic code.

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -2,8 +2,25 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("FizzBuzz goes here");
+		for(int i = 1; i <= 100; i++)
+		{
+			if(i % 3 == 0 && i % 5 == 0)
+			{
+				System.out.println("FizzBuzz");
+			}
+			else if(i % 3 == 0)
+			{
+				System.out.println("Fizz");
+			}
+			else if(i % 5 == 0)
+			{
+				System.out.println("Buzz");
+			}
+			else
+			{
+				System.out.println(i);
+			}
+		}
 	}
 
 }
