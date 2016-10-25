6fe548d0beebbbe1d4935f233c765c89fa93dc04
John Kelley <scouting4jesus@gmail.com>
2016-10-25 15:21:30 -0700

    HW 6
    Update change finder

--- HW 6/src/changeFinder/Test.java
+++ HW 6/src/changeFinder/Test.java
@@ -3,8 +3,6 @@ package changeFinder;
 public class Test {
 	public static void main(String[] agrs) {
 		ChangeFinder change = new ChangeFinder();
-		change.findChange(1.25);
-		change.findChange(0.75);
-		change.findChange(0.47);
+		change.findChange(1);
 	}
 }
