0bf6015909a470c9b9b69f2b1daba22d0f0e1c85
John Kelley <scouting4jesus@gmail.com>
2016-10-29 21:31:53 -0700

    HW 7
    First commit

---
+++ HW 7/src/morseCodeProcessor/MorseCodeProcessor.java
@@ -0,0 +1,10 @@
+package morseCodeProcessor;
+import binaryTree.MorseCodeTree;
+
+public class MorseCodeProcessor {
+	public static void main(String[] args){
+		MorseCodeTree mct = new MorseCodeTree();
+		mct.buildFromFile();
+	}
+
+}
